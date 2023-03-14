1a3459ce51e50458957f073c642d6e77c178cbe3
unknown <romain.nourrydu66@gmail.com>
2023-03-14 18:46:43 +0100

    done

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const third = document.querySelector(".sidebar-element:nth-child(3)");
 const four = document.querySelector(".sidebar-element:nth-child(4)");
 const btn = document.getElementById("btn");
 
+let isAnimated = false;
+
 btn.addEventListener("click", () => {
-  // Ajouter une classe CSS pour activer l'animation
-  first.classList.add("animate");
-  second.classList.add("animate");
-  third.classList.add("animate");
-  four.classList.add("animate");
+  if (isAnimated) {
+    four.classList.remove("animate");
+    setTimeout(() => {
+      third.classList.remove("animate");
+    }, 100);
+    setTimeout(() => {
+      second.classList.remove("animate");
+    }, 200);
+    setTimeout(() => {
+      first.classList.remove("animate");
+    }, 300);
+    isAnimated = false;
+  } else {
+    first.classList.add("animate");
+    setTimeout(() => {
+      second.classList.add("animate");
+    }, 100);
+    setTimeout(() => {
+      third.classList.add("animate");
+    }, 200);
+    setTimeout(() => {
+      four.classList.add("animate");
+    }, 300);
+    isAnimated = true;
+  }
 });
